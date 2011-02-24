3112c1e1df7131520a13a98d8ea2ee245631e208
Arash Rouhani <miffoljud@gmail.com>
2011-02-24 11:34:23 +0100

    Added zerosum.m and dimension assertion

---
+++ zerosum.m
@@ -0,0 +1,10 @@
+function [ x, y ] = zerosum( n, m, P )
+%ZEROSUM Summary of this function goes here
+%   Detailed explanation goes here
+
+assert([n, m] == size(P), 'Dimensions mismatch');
+
+
+
+end
+
